f6179bca3ac44a90b1fd809efb3f44c0765da9eb
Florent Merlin <florentmerlin.pro@gmail.com>
2022-01-04 10:16:24 +0100

    deploy

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 require('dotenv').config();
 const expess = require('express');
-const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/dbConfig');
 const authRouter = require('./routes/authRouter');
 const alertRouter = require('./routes/alertRouter');
 const userRouter = require('./routes/userRouter');
 
-
 const app = expess();
-app.use(expess.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-
-
-app.use('/', authRouter);
-app.use('/', alertRouter);
-app.use('/', userRouter);
-
 const port = process.env.PORT || 3500;
 
 const start = async () => {
@@ -32,3 +21,12 @@ const start = async () => {
 };
 
 start();
+
+app.use(cors());
+app.use(expess.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', authRouter);
+app.use('/', alertRouter);
+app.use('/', userRouter);
